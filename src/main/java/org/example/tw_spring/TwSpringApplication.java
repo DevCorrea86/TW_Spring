63d47cc4c2c9cb136fb2c7989820cac7c779d9f0
CorreaBrunoMiguel <devsistemascorrea@gmail.com>
2024-04-30 12:12:49 -0300

    Init

---
+++ src/main/java/org/example/tw_spring/TwSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.tw_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwSpringApplication.class, args);
+	}
+
+}
